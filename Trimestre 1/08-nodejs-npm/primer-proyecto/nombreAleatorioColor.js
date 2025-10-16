53c2de104364c33f4650c4124d36e61806560fa3
BartlomiejKrzysztofChudy <chudy@adaits.es>
2025-10-16 18:23:23 +0200

    Clase 16/10/2025

---
+++ Trimestre 1/08-nodejs-npm/primer-proyecto/nombreAleatorioColor.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+import { faker } from '@faker-js/faker';
+
+const colores = [chalk.blue, chalk.red, chalk.yellow, chalk.green, chalk.gray];
+
+
+function nombreAleatorio() {
+
+    const nombre = faker.internet.username();
+    const color = colores[Math.floor(Math.random() * colores.length)];
+
+    console.log('Nombre:', color(nombre));
+}
+
+nombreAleatorio();
+
