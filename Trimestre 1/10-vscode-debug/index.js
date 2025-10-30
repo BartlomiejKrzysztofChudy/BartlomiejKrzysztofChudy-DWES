30e75df945bf01e414fe2f4fdcc9fcad6d4cdc14
BartlomiejKrzysztofChudy <chudy@adaits.es>
2025-10-30 09:58:07 +0100

    .

---
+++ Trimestre 1/10-vscode-debug/index.js
@@ -0,0 +1,10 @@
+import { saludo } from "./app.js";
+
+const nombre = "Bartolome";
+const edad = 21;
+const mensaje = saludo(nombre);
+
+debugger; 
+
+console.log(mensaje);
+console.log("Edad:", edad);
