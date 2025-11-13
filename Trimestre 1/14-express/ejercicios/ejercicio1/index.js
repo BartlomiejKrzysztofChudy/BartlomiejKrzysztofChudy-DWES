c4da95fd61d35e8414057c2b62e2454ca3a4e1bc
BartlomiejKrzysztofChudy <chudy@adaits.es>
2025-11-13 18:14:05 +0100

    .

---
+++ Trimestre 1/14-express/ejercicios/ejercicio1/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+const port = 3000; 
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo ');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recibida una solicitud POST');
+});
+
+app.put('/user', (req, res) => {
+  res.send('Recibida una solicitud PUT en /user');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Recibida una solicitud DELETE en /user');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
