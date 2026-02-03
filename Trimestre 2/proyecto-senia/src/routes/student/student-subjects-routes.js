de10a21ad5929161c9b4c47708e59c0b5027bee4
BartlomiejKrzysztofChudy <chudy@adaits.es>
2026-02-03 09:29:20 +0100

    Add subject detail endpoint for students

--- Trimestre 2/proyecto-senia/src/routes/student/student-subjects-routes.js
+++ Trimestre 2/proyecto-senia/src/routes/student/student-subjects-routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { getMySubjects } from "../../controllers/student/student-subjects-controller.js";
+import { getMySubjectDetail} from "../../controllers/student/student-subject-detail-controller.js";
 import authMiddleware from "../../middlewares/auth-middleware.js";
 import roleMiddleware from "../../middlewares/role-middleware.js";
 
@@ -10,5 +11,6 @@ router.use(authMiddleware);
 router.use(roleMiddleware("STUDENT"));
 
 router.get("/subjects", getMySubjects);
+router.get("/subjects/:subjectId", getMySubjectDetail);
 
 export default router;
